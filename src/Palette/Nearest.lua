872d6bb118e365ec6deec98d168ae5bebe2edaa5
Cam <21029087+csqrl@users.noreply.github.com>
2023-05-21 19:46:01 +0100

    Implement `Palette.Nearest`

---
+++ src/Palette/Nearest.lua
@@ -0,0 +1,30 @@
+--!strict
+local function nearest(to: Color3, colors: {Color3}): Color3
+    assert(typeof(to) == "Color3", `Nearest(...): Expected "to" to be a Color3, got "{to}" ({typeof(to)})`)
+
+    local smallestMagnitude: number = math.huge
+    local nearestColor: Color3 = nil
+    local didWarn = false
+
+    for index, color in colors do
+        if typeof(color) ~= "Color3" then
+            if didWarn == false then
+                warn(`Nearest(...): Expected "colors" to be an array of Color3, got "{color}" ({typeof(color)}) at index #{index}`)
+                didWarn = true
+            end
+
+            continue
+        end
+
+        local magnitude = math.sqrt((color.R - to.R) ^ 2 + (color.G - to.G) ^ 2 + (color.B - to.B) ^ 2)
+
+        if magnitude < smallestMagnitude then
+            smallestMagnitude = magnitude
+            nearestColor = color
+        end
+    end
+
+    return nearestColor
+end
+
+return nearest
---
+++ src/Palette/Nearest.spec.lua
@@ -0,0 +1,46 @@
+local nearest = require(script.Parent.Nearest)
+
+local TEST_COLORS = {
+    OBSIDIAN = Color3.fromHex("#04040C"),
+    DANGER = Color3.fromHex("#FC0404"),
+    ORANGE = Color3.fromHex("#F26E1F"),
+    GOLD = Color3.fromHex("#E5CF18"),
+    FOREST = Color3.fromHex("#7A9C28"),
+    SKY = Color3.fromHex("#00A2FF"),
+    LAVENDER = Color3.fromHex("#7D5CA4"),
+    BUBBLEGUM = Color3.fromHex("#FC628F"),
+    SNOW = Color3.fromHex("#CCD6F4"),
+}
+
+local COLOR = {
+    BLACK = Color3.new(0, 0, 0),
+    RED = Color3.new(1, 0, 0),
+    GREEN = Color3.new(0, 1, 0),
+    BLUE = Color3.new(0, 0, 1),
+    WHITE = Color3.new(1, 1, 1),
+}
+
+local colorArray = {}
+
+for _, color in TEST_COLORS do
+    table.insert(colorArray, color)
+end
+
+return function()
+    it("returns a Color3", function()
+        local result = nearest(COLOR.BLACK, colorArray)
+        expect(typeof(result)).to.equal("Color3")
+    end)
+
+    it("returns the nearest Color3", function()
+        local result0 = nearest(COLOR.WHITE, colorArray)
+        local result1 = nearest(COLOR.BLUE, colorArray)
+        local result2 = nearest(COLOR.GREEN, colorArray)
+        local result3 = nearest(COLOR.RED, colorArray)
+
+        expect(result0).to.equal(TEST_COLORS.SNOW)
+        expect(result1).to.equal(TEST_COLORS.SKY)
+        expect(result2).to.equal(TEST_COLORS.FOREST)
+        expect(result3).to.equal(TEST_COLORS.DANGER)
+    end)
+end
