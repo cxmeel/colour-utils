d43415e8b9ce36cc225eecd25fae13603133872f
csqrl <21029087+csqrl@users.noreply.github.com>
2023-07-10 11:45:01 +0100

    Update workflows for aftman

--- .vscode/scripts/run-tests.ps1
+++ .vscode/scripts/run-tests.ps1
@@ -1,6 +1,6 @@
 $FILE_NAME = "test-place"
 
-foreman install
+aftman install
 rojo build .\test.project.json -o ".\$FILE_NAME.rbxl"
 run-in-roblox --place ".\$FILE_NAME.rbxl" --script ".\test-runner.server.lua"
 Remove-Item -Force ".\$FILE_NAME.rbxl"
