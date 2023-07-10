2de57c2aebf5f54c8228e831a72fb904cf0b50b1
csqrl <21029087+csqrl@users.noreply.github.com>
2023-07-10 11:45:15 +0100

    Update test-runner.server.lua

--- test-runner.server.lua
+++ test-runner.server.lua
@@ -1,8 +1,8 @@
 local ServerScripts = game:GetService("ServerScriptService")
 local TestService = game:GetService("TestService")
 
-local TestEZ = require(TestService.Packages.TestEZ)
+local TestEZ = require(TestService:WaitForChild("Packages", 10).TestEZ)
 
 TestEZ.TestBootstrap:run({
-    ServerScripts.ColourUtils,
+	ServerScripts.ColourUtils,
 })
